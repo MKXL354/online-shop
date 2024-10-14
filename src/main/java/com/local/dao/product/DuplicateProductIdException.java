5fe9de1e73f391c033785f255e50f784b42e3700
MKXL354 <mahdykamali354@yahoo.com>
2024-10-14 13:31:36 +0330

    feat: added error response handling to implemented product services

--- src/main/java/com/local/dao/product/DuplicateProductIdException.java
+++
@@ -1,7 +0,0 @@
-package com.local.dao.product;
-
-public class DuplicateProductIdException extends ProductDAOException{
-    public DuplicateProductIdException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
