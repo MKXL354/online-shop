3d420275b548ef4b2f92986a66bcbe628781316f
MKXL354 <mahdykamali354@yahoo.com>
2024-11-27 10:30:26 +0330

    feat: transaction manager ready to deploy. MemDAO must be made compliant

--- src/main/java/com/local/util/transaction/TransactionException.java
+++ src/main/java/com/local/util/transaction/TransactionException.java
@@ -1,8 +1,8 @@
 package com.local.util.transaction;
 
-import com.local.exception.common.ApplicationException;
+import com.local.exception.common.ApplicationRuntimeException;
 
-public class TransactionException extends ApplicationException {
+public class TransactionException extends ApplicationRuntimeException {
     public TransactionException(String message, Throwable cause) {
         super(message, cause);
     }
